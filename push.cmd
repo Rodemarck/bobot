e3001e70f4dc3cd6dd968ff814849ae47b286c4d
rodemarck <rodemarck.meloj@ufrpe.br>
2021-03-18 00:36:32 -0300

    'aaa'

--- push.cmd
+++ push.cmd
@@ -1,7 +1,12 @@
 @echo off
 cd build
 cd libs
-if exist "libs.zip" del "libs.zip"
-7z a -tzip libs.zip rode-all.jar ./../../.env ./../../src/main/resources
+if exist libs.zip del libs.zip
+if exist "resources" del "resources"
+mkdir "resources"
+copy ./../../src/main/resources/messages_en_US.properties resources/messages_en_US.properties
+copy ./../../src/main/resources/messages_pr_BR.properties resources/messages_pt_BR.properties
+7z a -tzip libs.zip rode-all.jar ./../../.env ./resources
+del "resources"
 cd..
 cd..
